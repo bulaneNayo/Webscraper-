fd3daf3195944e5c1ffa0588b55e249bb7fcbcec
bulaneNayo <bulane.naeo@bothouniversity.com>
2021-08-26 10:49:38 +0200

    Webscraper task

--- webscraper.js
+++ webscraper.js
@@ -1,45 +1,57 @@
-//import Puppeteer from 'puppeteer';
-import  React from 'react'
+//required modules    
+import cheerio from "cheerio";
+import got from "got";
+import PromptSync from "prompt-sync";
+import puppeteer from "puppeteer";
 
-const puppeteer = require('puppeteer')
-async () => {
+async function scrapeData() {
+  
+  try {
+
+        //prompts the user to enter the url of the website on the terminal
+      const prompt =  PromptSync();
+      let WebSite = prompt("State the website to scrape: ");  
+  
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();    //Promise which resolves to a new Page object.
+
+      const url = WebSite; 
+      await page.goto(url);  //Navigates to a URL 
+  
+      //Scrape the title of the page
+      const h1 = await page.evaluate(
+      () => document.querySelector("h1").textContent
+      );
+
+      //Displays the title of the page on the terminal
+      console.log(h1);
+
+      //Scrape image links from the page
+        const images = await page.evaluate(() =>
+        Array.from(document.querySelectorAll("img"))
+        .map(logo =>logo.src)
+
+        )
+
+      //Desplay images links on the terminal 
+      console.log(images);
+
+      await got(url).then(response => {
+      const $ = cheerio.load(response.body);
     
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    
-    await page.goto('https://www.geeksforgeeks.org/java/');
+      $('a').each((i, link) => {
+        const href = link.attribs.href; 
 
-    const [Image] = await page.$x('//*[@id="post-126247"]/div/p[1]/img');
-    const sourceAttribube = await Image.getProperty('sourceAttribube');
-    const Text = await sourceAttribube.jsonValue();
-
-   console.log({Text});
-
-   browser.close(); 
-   //console.log(`Site HTML: ${$.html()}\n\n`)   
+        console.log(href); 
+  
+  })
+      //Closes browser with all the pages opened.  
+      browser.close();
+})
+}     catch (error) {
+      console.error(error);
 }
-//fetchHTML('https://www.geeksforgeeks.org/java/')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}  
+   
+// Invoke the above function
+scrapeData();
